9e27a36d7478b682809c478055c7c1325cd2dcad
valeriocorp <franciscojvg0607@gmail.com>
2022-06-27 12:05:49 -0500

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:rxdart/rxdart.dart';
+
+import 'views/home_page.dart';
 
 
 void main() {
@@ -21,41 +21,5 @@ class MyApp extends StatelessWidget {
       ),
       home: const HomePage(),
     );
-  }
-}
-
-class HomePage extends HookWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    //Create our behavior subject every time the widget is built.
-    final subject = useMemoized(
-      () => BehaviorSubject<String>(),
-      [key],
-      );
-    //dispose of the old subject when the widget is rebuilt.  
-    useEffect(
-      ()=> subject.close,
-      [subject],
-    );  
-    return Scaffold(
-      appBar: AppBar(
-        title: StreamBuilder<String>(
-          stream: subject.stream.distinct().debounceTime(const Duration(seconds: 1)),
-          initialData: 'Please start typing...',
-          builder: (context, snapshot) {
-            return Text(snapshot.requireData); 
-          },
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: TextField(
-          onChanged: subject.sink.add,
-        ),
-      ),
-      
-      );
   }
 }
---
+++ lib/models/person.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:testingrxdart/models/thing.dart';
+
+@immutable
+class Person extends Thing{
+  final int age;
+ const Person({required String name, required this.age}) : super(name: name);
+
+  @override
+  String toString() {
+    return 'Person, name: $name, age: $age';
+  }
+
+  Person.fromJson(Map<String, dynamic> json) :
+   age= json['age'] as int,
+   super(name: json['name'] as String);
+}
